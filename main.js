54854d7a9c8d04046cf8332a98a3501c9c806a3b
zaid123456788889 <92422163+zaid123456788889@users.noreply.github.com>
2021-10-13 11:19:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function Login(){
+    player1_name = document.getElementById("player_1_input");
+    player2_name = document.getElementById("player_2_input");
+
+
+    window.location = "index_2.html";
+}
